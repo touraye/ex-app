5b7b39b38305fc78f7fa919923664325d7ea97d7
touraye <touraye07@gmail.com>
2022-08-16 11:29:37 +0000

    creating history

--- client/src/components/History.jsx
+++ client/src/components/History.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
-
-export const History = () => {
+export const History = ( { transactions } ) => {
+  const trans = transactions?.map( trans => trans )    
+  const lastTran = trans[ trans.length - 1 ]
+  
   return (
-    <div className='history'>
-      <h3>history</h3>
-      <ul className='list'>
-
-      </ul>
-    </div>
-  )
+		<div className='history'>
+			<h3>latest transaction</h3>
+			<div className='history-list'>
+				<div className='flex-1'>
+					<p>{lastTran?.name}</p>
+					<p className={`${lastTran?.type === 'income' ? 'income' : 'expense'}`}>GMD {lastTran?.amount}</p>
+				</div>
+				<div>
+					<p>{lastTran?.type}</p>
+					<p>{new Date(lastTran?.createdAt).toLocaleString('en-US')}</p>
+				</div>
+			</div>
+		</div>
+	)
 }
