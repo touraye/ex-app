cdb9eb94ff79799621dc7ebcc38cf7f0eb13497d
touraye <touraye07@gmail.com>
2022-09-07 08:48:26 +0000

    updating footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,37 +1,55 @@
+import { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
 import { FaUser, FaHome, FaMoneyCheck, FaList, FaChartBar } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 
 const Footer = () => {
-  return (
-		<footer className='footer'>
-			<ul>
-				<li>
-					<Link to='/'>
-						<FaHome />
-					</Link>
-				</li>
-				<li>
-					<Link to='/users'>
-						<FaList />
-					</Link>
-				</li>
-				<li>
-					<Link to='/transactions'>
-						<FaMoneyCheck />
-					</Link>
-				</li>
-				<li>
-					<Link to='/statistic'>
-						<FaChartBar />
-					</Link>
-				</li>
-				<li>
-					<Link to='/account'>
-						<FaUser />
-					</Link>
-				</li>
-			</ul>
-		</footer>
+	const { auth } = useSelector( ( state ) => state )
+	
+	const [ showMenu, setShowMenu ] = useState( false )
+
+	useEffect( () => {
+		if ( auth ) {
+			setShowMenu(true)
+		}
+	}, [auth])
+	
+	return (
+		<>
+			{
+				showMenu &&
+				<footer className='footer'>
+					<ul>
+						<li>
+							<Link to='/'>
+								<FaHome />
+							</Link>
+						</li>
+						<li>
+							<Link to='/users'>
+								<FaList />
+							</Link>
+						</li>
+						<li>
+							<Link to='/transactions'>
+								<FaMoneyCheck />
+							</Link>
+						</li>
+						<li>
+							<Link to='/statistic'>
+								<FaChartBar />
+							</Link>
+						</li>
+						<li>
+							<Link to='/account'>
+								<FaUser />
+							</Link>
+						</li>
+					</ul>
+				</footer>
+			
+			}
+		</>
 	)
 }
 
