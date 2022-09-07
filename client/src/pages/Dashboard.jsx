a92b263c76159eb9e14c81725a75e4e57f708561
touraye <touraye07@gmail.com>
2022-09-07 08:56:01 +0000

    creating dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,16 +1,12 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import AddTransaction from '../components/AddTransaction'
 import { History } from '../components/History'
-import { getTransactions, reset } from '../features/transaction/transactionSlice'
-
+import { getTransactions } from '../features/transaction/transactionSlice'
+import { curr } from '../helpers'
 const Dashboard = () => {
-  const { transactions, isLoading, isMessage, isSuccess, isError } = useSelector( ( state ) => state.transaction )
-  const { users } = useSelector((state) => state.user)
+  const { transactions, isLoading, isMessage, isError } = useSelector( ( state ) => state.transaction )  
   const dispatch = useDispatch()
-	const [ show, setShow ] = useState( false )
-
-
 	
 	useEffect( () => {
 		if ( isError ) {
@@ -19,29 +15,25 @@ const Dashboard = () => {
 		dispatch(getTransactions())
 	}, [ dispatch, isMessage, isError ] )
 
-		const income = transactions?.map((trans) => trans.amount)			
-		const currIncome = income.length > 0 && income.reduce((acc, cur) => acc + cur)
-		const expense = transactions
-			?.filter((trans) => trans.type === 'expense')
-			.map((trans) => trans.amount)			
-	const currExpense = expense.length > 0 && expense.reduce((acc, cur) => acc + cur)
-	const balance = currIncome - currExpense
-	console.log('income', income, 'expense', expense, 'balance', balance)
+	const totalIncome = curr(transactions)[0]
+	const totalBalance = curr(transactions)[1]
+	const totalExpense = curr( transactions )[ 2 ]
+
 	
 	if(isLoading) <h2>...loading</h2>
   
 	return (
 		<div>
 			<h4 className='balance-text'>your balance</h4>
-			<p className='balance'>GMD {balance ? balance : '0.00'}</p>
+			<p className='balance'>GMD {totalBalance ? totalBalance : '0.00'}</p>
 			<div className='inc-exp-container'>
 				<div>
 					<h4>Income</h4>
-					<p className='money plus'>GMD {currIncome ? currIncome : '0.00'}</p>
+					<p className='money plus'>GMD {totalIncome ? totalIncome : '0.00'}</p>
 				</div>
 				<div>
 					<h4>Expense</h4>
-					<p className='money minus'>GMD {currExpense ? currExpense : '0.00'}</p>
+					<p className='money minus'>GMD {totalExpense ? totalExpense : '0.00'}</p>
 				</div>
 			</div>
 			<History transactions={transactions} />
