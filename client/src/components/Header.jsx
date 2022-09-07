c56a5218ed97c71906bf6497bbac0b625a3f0656
touraye <touraye07@gmail.com>
2022-09-07 08:48:57 +0000

    updating header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,60 +1,50 @@
 import { Link, useNavigate } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 import { useEffect } from 'react'
-import { FaUser, FaSignInAlt, FaSignOutAlt } from 'react-icons/fa'
-import { logout, reset } from '../features/auth/authSlice'
+import { FaBitcoin, FaCalculator, FaMoneyBill, FaSignInAlt, FaSignOutAlt } from 'react-icons/fa'
+import { setLoggedOut } from '../features/auth/authSlice'
 
 const Header = () => {    
-  const { auth, isLoading, isSuccess, isMessage, isError, } = useSelector( ( state ) => state.auth )  
+  const { auth } = useSelector( ( state ) => state )  
   const navigate = useNavigate()
   const dispatch = useDispatch()
 
-  useEffect( () => {
-    if ( isError ) {
-      alert(isMessage);
-    }   
-
-    if ( !auth ) {
+  useEffect( () => {      
+    if ( auth === null ) {
       navigate('/login')
     }
-
-    //   if ( auth ) {
-    //   navigate('/')
-    // }
-  }, [ isError, auth, navigate, isSuccess, isMessage ] )
+  }, [ auth, navigate ] )
   
-  const onLogout = () => {
-    dispatch( logout() )
-    dispatch( reset() )
+  const onLogout = () => {    
+    window.localStorage.clear()
+    dispatch(setLoggedOut())    
     navigate('/login')
-  }
-
-  if(isLoading) return <h1>Loading...</h1>
+  }  
 
   return (
-    <header className='header'>
-      <div className="logo">
-        <Link to='/'>
-          Expense App
-        </Link>
-      </div>      
-      <ul>
-        {
-          auth ?
-            <li>   
-              {auth?.username}  
-              <button onClick={onLogout}>
-                <FaSignOutAlt /> logout
-              </button>            
-          </li>
-            :
-          <li>
-            <FaSignInAlt /> login
-          </li>
-        }
-      </ul>
-    </header>
-  )
+		<header className='header'>
+      <div className='logo'>
+        <FaMoneyBill className='icon logo-icon' />
+				<Link to='/'>Expense App</Link>
+			</div>
+			<ul>
+				{auth ? (
+					<li className='loggedUser-list'>
+						<p>{auth?.username}</p>
+						<button onClick={onLogout} className='btn logout-btn logout-btn-sm'>
+							<FaSignOutAlt className='icon logout-icon-ex-small' /> logout
+						</button>
+					</li>
+				) : (
+					<li>
+						<button className='btn logout-btn logout-btn-sm'>
+							<FaSignInAlt className='icon logout-icon-ex-small' /> login
+						</button>
+					</li>
+				)}
+			</ul>
+		</header>
+	)
 }
 
 export default Header
