1b26c215a2285228a7d81db7be0d323aed6fd038
touraye <touraye07@gmail.com>
2022-09-20 21:14:56 +0000

    stats

--- client/src/pages/Chart.jsx
+++ client/src/pages/Chart.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import IncomeExpenseBar from '../components/Chart/IncomeExpenseBar'
 import IncomeExpenseLine from '../components/Chart/IncomeExpenseLine'
 import MonthlyTransaction from '../components/Chart/MonthlyTransaction'
+import YearlyTransaction from '../components/Chart/YearlyTransaction'
 const Chart = () => {
   return (
 		<div>
 			<IncomeExpenseBar />
 			<IncomeExpenseLine />
 			<MonthlyTransaction />
+			<YearlyTransaction />
 		</div>
 	)
 }
